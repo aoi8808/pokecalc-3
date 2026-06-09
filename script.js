5bea0decca7b099968e3719a156255b13b01923b
aoi8808 <blueblue20051110@icloud.com>
2026-06-09 13:38:41 +0900

    Update script.js

--- script.js
+++ script.js
@@ -312,13 +312,12 @@ function updateAttacker(){
         // 見つからなかった場合（新メガシンカなど）のエラー対策
         img.onerror = function() {
         // 自分のパソコン（GitHub）の images フォルダの画像を代わりに表示する
-        this.src = `./images/${pokemonId}.png`;
+        this.src = `./${pokemonId}.png`;
     
-        // それでも無ければ「画像なし」アイコンにする
-        this.onerror = function() {
-        this.src = "./images/no_image.png"; 
-    }
-};
+        img.onerror = function() {
+            this.src = ""; 
+            this.alt = "画像なし";
+        };
         // --- ステータスの計算 ---
         let statType = attackTypeSelect.value;
         let baseAttack = selectedPokemon[statType];
@@ -382,13 +381,12 @@ function updateDefender(){
         // 見つからなかった場合（新メガシンカなど）のエラー対策
         img.onerror = function() {
         // 自分のパソコン（GitHub）の images フォルダの画像を代わりに表示する
-        this.src = `./images/${pokemonId}.png`;
+        this.src = `./${pokemonId}.png`;
     
-        // それでも無ければ「画像なし」アイコンにする
-        this.onerror = function() {
-        this.src = "./images/no_image.png"; 
-    }
-};
+        img.onerror = function() {
+            this.src = ""; 
+            this.alt = "画像なし";
+        };
         
         // --- ステータスとHPの計算 ---
         let defStatType = defenceTypeSelect.value;
